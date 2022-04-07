c36a021706df39322b8b8fc0770c5694c368f204
Ramses Marrero <ramsesmg1992@gmail.com>
2022-04-07 10:51:12 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+
+## Importar la data
+df = pd.read_csv('data/Pzero_dummies.csv', delimiter = ';')
+
+#Crear una tabla dinámica
+pv = pd.pivot_table(df, index=['paciente'], columns=["NRL"], values=['Cr'], aggfunc=sum, fill_value=0)
+
+app = dash.Dash()
+
+app.layout = html.Div(children=[
+    html.H1(children='Hola Mundo'),
+    html.Div(children='<p>Prueba proyecto AVATAR</p>')
+])
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
